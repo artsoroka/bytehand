cb9bea97ce2654f09881a20bebad5aa54f75fb42
artsoroka <githubrepository@yandex.ru>
2015-04-22 13:40:39 +0000

    added function _call() to handle common request scenarious

--- bytehand.js
+++ bytehand.js
@@ -1,42 +1,52 @@
+
 var request     = require("request"); 
 var querystring = require("querystring"); 
-var extend      = require('util')._extend
+var extend      = require('util')._extend; 
 var API_URL     = "http://bytehand.com:3800"; 
 
 var Bytehand = function(config){
     this.auth = {
         id: config.id || null, 
         key: config.key || null
-    }
-}
+    };
+}; 
 
 module.exports = function(config){
     var config = config || {}; 
     return new Bytehand(config); 
-}
+}; 
 
 Bytehand.prototype.send = function(message, callback){
-    var url  = this.prepareUrl('send', message); 
+    this._call('send', message, function(err, response){
+       if( err ) return callback(err, null); 
+       
+       callback(null, {
+           status: "OK", 
+           messageId: response.description
+       }); 
+       
+    });
+}; 
+
+
+Bytehand.prototype._call = function(method, message, callback){
+    var url  = this.prepareUrl(method, message); 
     var self = this; 
     request.get(url, function(err, response, body){
         self._handle(err, response, body, callback); 
     }); 
-    
-}
+}; 
 
 Bytehand.prototype._handle = function(err, response, body, callback){
     if( err) return callback(err, null);
     
     var data = this._parseJSON(body); 
     
-    if( response.statusCode != 200 || data.status != 0) 
+    if( response.statusCode != 200 || data.status !== 0) 
         return callback(data, null); 
-    
-    callback(null, {
-        status: 'OK', 
-        messageId: data.description
-    });
-}
+
+    callback(null, data);
+}; 
 
 Bytehand.prototype._parseJSON = function(str){
     var data = null;  
@@ -46,11 +56,11 @@ Bytehand.prototype._parseJSON = function(str){
         console.log('could not parse JSON string: ', str, e); 
     }
     return data; 
-}
+}; 
 
-Bytehand.prototype.prepareUrl = function(method, params){
-    params  = extend(this.auth, params); 
-    var qs  = [method, querystring.stringify(params)].join('?'); 
+Bytehand.prototype.prepareUrl = function(method, message){
+    var params  = extend(message, this.auth); 
+    var qs      = [method, querystring.stringify(params)].join('?'); 
     
     return [API_URL, qs].join('/'); 
-}
+}; 
